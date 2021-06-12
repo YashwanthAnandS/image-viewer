5ec7a3e4a3e388abe1e3cd5ce0beab35a170f1b8
Yashwanth Anand S <yashwanthanands@gmail.com>
2021-06-12 21:29:29 +0530

    Commit with Part 1 - Login Features and Functionalities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './screens/login/Login'; 
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    // <span>
+    //     Image Viewer
+    // </span>, 
+    <Login/>,
     document.getElementById('root')
 );
